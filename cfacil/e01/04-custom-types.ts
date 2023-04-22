786aaaf8113f11cc489a3333d6ab5ede7a91d4b8
Alfonso Baqueiro Bernal <abaqueiro@gmail.com>
2023-04-22 02:52:45 +0000

    Some examples about definition and usage of custom types.
    	new file:   04-custom-types.ts
    	modified:   package.json

---
+++ cfacil/e01/04-custom-types.ts
@@ -0,0 +1,39 @@
+
+// you can create a new data type (data structure like)
+type Persona = {
+	nombre: string
+	apaterno: string
+	amaterno: string
+	comentarios?: string //optional
+}
+
+var unaPersona: Persona = {
+	nombre: 'Alfonso'
+	, apaterno: 'Baqueiro'
+	, amaterno: 'Bernal'
+}
+
+console.log( typeof unaPersona )
+console.log( JSON.stringify(unaPersona) )
+
+// declare several valid types for a ServiceResponse
+type ServiceResponse = string | null | number | undefined
+
+// declare several valid values for user types
+type UserType = 'admin' | 'normal' | 'superuser'
+
+type User = {
+	persona: Persona
+	type: UserType
+}
+
+let someUser: User = {
+	persona: {
+		nombre: 'Liam'
+		, apaterno: 'Baqueiro'
+		, amaterno: 'Flores'
+	}
+	, type: 'superuser'
+}
+console.log( JSON.stringify( someUser ) )
+
